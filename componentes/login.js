b35d3edbb627ca218c71e4d6894bdfcb345a8635
JoshuaB-01 <joshuablua@gmail.com>
2024-07-01 21:12:51 -0300

    Ultima entrega

--- componentes/login.js
+++ componentes/login.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
 
-    loginForm.addEventListener('submit', (event) => {
+        loginForm.addEventListener('submit', (event) => {
         event.preventDefault(); 
         
         const email = document.querySelector('input[name="email"]').value;
         const pass = document.querySelector('input[name="pass"]').value;
 
-        let usuarios = JSON.parse(sessionStorage.getItem('usuariosRegistrados')) || [];
-        const usuario = usuarios.find(user => user.email === email && user.pass === pass);
-
-        if (usuario) {
-           
-            let usuariosActivos = JSON.parse(sessionStorage.getItem('usuariosActivos')) || [];
-            usuariosActivos.push({ email: usuario.email, pass: usuario.pass });            
-            sessionStorage.setItem('usuariosActivos', JSON.stringify(usuariosActivos));      
+        fetch('../componentes/usuarios.json') .then(response => response.json()).then(usuarios => {
             
-            window.location.href = '../private_page/home.html';
-        } else {
-            alert('Email o contraseña incorrectos');
-        }
+            const usuario = usuarios.find(user => user.email === email && user.password === pass);
+
+            if (usuario) {
+                window.location.href = '../private_page/home.html';
+            } else {
+                alert('Email o contraseña incorrectos');
+            }
+        })
+        .catch(error => {
+            console.error('Error de usuarios', error);
+        });
     });
 });
--- items/compras.js
+++ items/compras.js
@@ -1,6 +1,6 @@
 import { navbarComponent } from "../componentes/navbar.js";
 import { footerComponent } from "../componentes/footer.js";
-import { getData } from "../items/localStorage.controller.js"; 
+import { getData, deleteCollection } from "../items/localStorage.controller.js"; 
 
 let navContainer = document.querySelector('header');
 let footerContainer = document.querySelector('footer');
@@ -21,8 +21,8 @@ window.addEventListener('load', () => {
     });
 
     const carrito = getData('carrito');
-    let tableHTML = `
-        <table class="table table-dark table-striped">
+    let tableHTML = 
+        `<table class="table table-dark table-striped">
             <thead>
                 <tr>
                     <th>Producto</th>
@@ -33,15 +33,35 @@ window.addEventListener('load', () => {
             <tbody>`;
     
     carrito.forEach(item => {
-        tableHTML += `
-            <tr>
+        tableHTML += 
+            `<tr>
                 <td>${item.titulo}</td>
                 <td>${item.cantidad}</td>
                 <td>$${item.precio}</td>
             </tr>`;
     });
-    tableHTML += `
-            </tbody>
-        </table>`;
+    tableHTML += 
+            `</tbody>
+        </table>
+        <div class="d-flex justify-content-end mb-3">
+            <button id="reinicioTabla" class="btn btn-danger me-2">Vaciar Carrito</button>
+            <button id="compraFinalizada" class="btn btn-success">Finalizar Compra</button>
+        </div>`;
+    
     tableContainer.innerHTML = tableHTML;
+
+    
+    const reinicioTabla = document.getElementById('reinicioTabla');
+    reinicioTabla.addEventListener('click', () => {
+        deleteCollection('carrito');
+        alert('Se vacio el carrito');
+        window.location.reload();
+    });
+    
+    const compraFinalizada = document.getElementById('compraFinalizada');
+    compraFinalizada.addEventListener('click', () => {
+        deleteCollection('carrito');
+        alert('Compra finalizada');
+        window.location.reload();
+    });
 });
